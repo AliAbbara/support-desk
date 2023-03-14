a51ae8f199081b84ee2bb035c59bdee82f7ea4ff
AliAbbara <abbara21@gmail.com>
2023-03-14 15:54:00 -0300

    Register and Login wiht database funcionality

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,16 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { registerUser, loginUser } = require('../controllers/usercontroller');
+const { registerUser, loginUser } = require('../controllers/userController');
 
 router.post('/', registerUser);
-router.post('/', loginUser);
-
-router.post('/', (req, res) => {
-  res.send('Register Route');
-});
-
-router.post('/login', (req, res) => {
-  res.send('Login Route');
-});
+router.post('/login', loginUser);
 
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,25 @@
-const express = require('express');
-const dotenv = require('dotenv').config();
-const PORT = process.env.PORT || 5000;
-const app = express();
+const express = require("express")
+const colors = require("colors")
+const dotenv = require("dotenv").config()
+const PORT = process.env.PORT || 5000
+const { errorHandler } = require("./middleware/errorMiddleware")
+const connectDB = require("./config/db")
 
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Welcome to asdasd' });
-});
+//Connect to database
+connectDB()
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Welcome to Support Desk API" })
+})
 
 //Routes
-app.use('/api/users', require('./routes/userRoutes'));
+app.use("/api/users", require("./routes/userRoutes"))
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.use(errorHandler)
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
