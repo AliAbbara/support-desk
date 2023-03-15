f265f118b6fcacc50022c53b25f716586b2a3593
AliAbbara <abbara21@gmail.com>
2023-03-15 16:06:28 -0300

    createTicket and geTicket done

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,9 @@
-const express = require("express")
-const colors = require("colors")
-const dotenv = require("dotenv").config()
+const express = require('express')
+const colors = require('colors')
+const dotenv = require('dotenv').config()
 const PORT = process.env.PORT || 5000
-const { errorHandler } = require("./middleware/errorMiddleware")
-const connectDB = require("./config/db")
+const { errorHandler } = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
 
 //Connect to database
 connectDB()
@@ -13,12 +13,13 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-app.get("/", (req, res) => {
-  res.status(200).json({ message: "Welcome to Support Desk API" })
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Welcome to Support Desk API' })
 })
 
 //Routes
-app.use("/api/users", require("./routes/userRoutes"))
+app.use('/api/users', require('./routes/userRoutes'))
+app.use('/api/tickets', require('./routes/ticketRoutes'))
 
 app.use(errorHandler)
 
